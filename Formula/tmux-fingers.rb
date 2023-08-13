8f5dae19b72f079fbe20972fbaa8087f1b149e2c
Jorge Morante <jorge@morante.eu>
2023-08-13 17:16:37 +0200

    Create morantron/tmux-fingers tap

---
+++ Formula/tmux-fingers.rb
@@ -0,0 +1,33 @@
+# Formula/tmux-fingers.rb
+
+class TmuxFingers < Formula
+  desc "mouseless terminal interaction"
+  homepage "https://github.com/morantron/tmux-fingers"
+  url "https://github.com/Morantron/tmux-fingers/archive/refs/tags/2.0.0-alpha1.tar.gz"
+
+  # run `brew fetch coveralls` to check SHA256
+  sha256 "9a57c3acb39427e75351bd4f228dadbe7caa886b350adac06df96c8723d7bcea"
+  license "MIT"
+
+  # use :build, so if there's a bottle it will be used
+  depends_on "crystal" => :build
+
+  # dependencies of Crystal
+  depends_on "bdw-gc"
+  depends_on "libevent"
+  depends_on "libyaml"
+  depends_on "pcre2"
+
+  # installation instructions
+  def install
+    system "shards", "install", "--production"
+    system "shards", "build", "--production"
+    system "strip", "./bin/tmux-fingers"
+    bin.install "./bin/tmux-fingers"
+  end
+
+  # testing if binary is OK
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tmux-fingers show-version").chomp
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Morantron Tmux-fingers
+
+## How do I install these formulae?
+
+`brew install morantron/tmux-fingers/<formula>`
+
+Or `brew tap morantron/tmux-fingers` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
