908b510e231fba4f03e0f0f55adf1d3f75924e82
Morantron <jorge@morante.eu>
2026-01-27 10:02:41 +0100

    tmux-fingers 2.6.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tmux-fingers.rb
+++ Formula/tmux-fingers.rb
@@ -3,10 +3,10 @@
 class TmuxFingers < Formula
   desc "mouseless terminal interaction"
   homepage "https://github.com/morantron/tmux-fingers"
-  url "https://github.com/Morantron/tmux-fingers/archive/refs/tags/2.6.0.tar.gz"
+  url "https://github.com/Morantron/tmux-fingers/archive/refs/tags/2.6.1.tar.gz"
 
   # run `brew fetch coveralls` to check SHA256
-  sha256 "5e41b21d0881d017c57cc09b8306b53a54419fa97ed29fc67456d786b7bf9abe"
+  sha256 "b4b2d4d75d32e5f6c0272cbc3e27a53a33aba6fea2f1addfb9885bdbf9010f6d"
   license "MIT"
 
   # use :build, so if there's a bottle it will be used
